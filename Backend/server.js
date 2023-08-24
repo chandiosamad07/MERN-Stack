4efa44221c6307e0af9e4d47fbf70fb8c86f5b6e
chandiosamad07 <absamadchandio@gmail.com>
2023-08-24 13:38:01 +0500

    postmen

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,10 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next)=>{
+console.log(req.path, req.method)
+next()
+})
 //routes
 app.get('/', (req,res)=>{
     res.json({msg:'Welcome to App'})
